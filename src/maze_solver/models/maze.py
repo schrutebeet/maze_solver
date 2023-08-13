8a9cc06a6a599c37e3f3c7ffb2730503844badb5
Ricardo Sans <ricardosans98@gmail.com>
2023-08-13 11:57:16 +0100

    Edit Maze class

--- src/maze_solver/models/maze.py
+++ src/maze_solver/models/maze.py
@@ -1,12 +1,21 @@
 from dataclasses import dataclass
+from typing import Iterator
 from maze_solver.models.square import Square
 
 @dataclass(frozen=True)
 class Maze:
     squares: tuple[Square, ...]
+    # this method will help in iterating over the object Maze.
+    # Needed in the next methods: 'width' and 'height'.
+    def __iter__(self) -> Iterator[Square]:
+        return iter(self.squares)
 
+    @property
     def width(self):
-        return max(square.column for square in self) + 1
+        width_list = [square.column for square in self]
+        return max(width_list) + 1
     
+    @property
     def height(self):
-        return max(square.row for square in self) + 1
+        height_list = [square.row for square in self]
+        return max(height_list) + 1
